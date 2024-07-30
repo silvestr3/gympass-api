8cc57affdaf5ec5ff99f356846ab2bc4697056df
silvestr3 <fellipe.silvestre@outlook.com>
2024-07-30 11:45:04 -0300

    feat: Implement role based access control rules

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -4,6 +4,7 @@ import { create } from "./create";
 import { validate } from "./validate";
 import { history } from "./history";
 import { metrics } from "./metrics";
+import { verifyUserRole } from "@/http/middlewares/verify-user-role";
 
 export async function checkInRoutes(app: FastifyInstance) {
   app.addHook("onRequest", verifyJWT);
@@ -12,5 +13,9 @@ export async function checkInRoutes(app: FastifyInstance) {
   app.get("/check-in/metrics", metrics);
 
   app.post("/gyms/:gymId/check-in", create);
-  app.patch("/check-in/:checkInId/validate", validate);
+  app.patch(
+    "/check-in/:checkInId/validate",
+    { onRequest: [verifyUserRole("ADMIN")] },
+    validate
+  );
 }
