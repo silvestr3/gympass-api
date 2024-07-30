3b1449605c8759c9978fe6bbf52172297d6c6c7f
silvestr3 <fellipe.silvestre@outlook.com>
2024-07-30 10:00:57 -0300

    test: Create separate test function to create and authenticate user

--- src/http/tests/profile.spec.ts
+++ src/http/tests/profile.spec.ts
@@ -1,6 +1,7 @@
 import { afterAll, beforeAll, describe, expect, it } from "vitest";
 import request from "supertest";
 import { app } from "@/app";
+import { createAndAuthenticateUser } from "@/utils/test/create-and-authenticate-user";
 
 describe("Profile (e2e)", async () => {
   beforeAll(async () => {
@@ -12,18 +13,7 @@ describe("Profile (e2e)", async () => {
   });
 
   it("Should be able to view user profile", async () => {
-    await request(app.server).post("/users").send({
-      name: "John Doe",
-      email: "johndoe@example.com",
-      password: "123456",
-    });
-
-    const authResponse = await request(app.server).post("/sessions").send({
-      email: "johndoe@example.com",
-      password: "123456",
-    });
-
-    const { token } = authResponse.body;
+    const { token } = await createAndAuthenticateUser(app);
 
     const profileResponse = await request(app.server)
       .get("/me")
