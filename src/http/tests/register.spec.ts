83ad041af6121c6396e2505c8ca4c5d0483fd078
silvestr3 <fellipe.silvestre@outlook.com>
2024-07-30 09:16:55 -0300

    test: Add e2e tests for register and authentication

--- src/http/tests/register.spec.ts
+++ src/http/tests/register.spec.ts
@@ -1,3 +1,23 @@
-import { test } from "vitest";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import request from "supertest";
+import { app } from "@/app";
 
-test("ok", () => {});
+describe("Register (e2e)", async () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("Should be able to register a new user", async () => {
+    const response = await request(app.server).post("/users").send({
+      name: "John Doe",
+      email: "johndoe@example.com",
+      password: "123456",
+    });
+
+    expect(response.statusCode).toEqual(201);
+  });
+});
